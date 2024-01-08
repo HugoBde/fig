3af31229df679d6c335aac5a9664572796590367
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-01-08 21:18:35 +1100

    some progress on the authentication process with instagram

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import './App.css'
 
+function LoginLink({ isLoggedIn }: { isLoggedIn: boolean }) {
+  if (isLoggedIn) {
+    return (
+      <a href="https://api.instagram.com/oauth/authorize?client_id=909105997317927&redirect_uri=https%3A%2F%2Flocalhost%3A5173%2F&scope=user_profile,user_media&response_type=code">login</a>
+    )
+  } else {
+    return null
+  }
+}
+
+function Foo({ code }: { code: string | null }) {
+  if (code) {
+    return (
+      <h1>
+        {code}
+      </h1>
+    )
+  } else {
+    return null;
+  }
+}
+
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get("code");
+
   return (
     <>
       <h1>Vite + React</h1>
+      <LoginLink isLoggedIn={!code} />
+      <Foo code={code} />
     </>
   )
 }
